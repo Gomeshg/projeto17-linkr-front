512aaf48c19ed139a3740a4cb2d13dc0b53cf323
Hugo Gomes <gomes.hugo00@gmail.com>
2022-10-21 11:46:15 -0300

    build: new branch trendings

--- src/components/pages/common/Trendings.js
+++ src/components/pages/common/Trendings.js
@@ -1,16 +1,13 @@
 import styled from "styled-components";
+// teste
 
 export default function Trendings() {
-    return (
-        <TrendingsStyle>
-
-        </TrendingsStyle>
-    );
-};
+  return <TrendingsStyle></TrendingsStyle>;
+}
 
 const TrendingsStyle = styled.div`
-    width: 300px;
-    height: 400px;
-    border-radius: 16px;
-    background-color: #171717;
+  width: 300px;
+  height: 400px;
+  border-radius: 16px;
+  background-color: #171717;
 `;
